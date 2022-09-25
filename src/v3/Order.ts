d430f973197bcba288f7b22e6f171b02cc242237
Jonathan Ferreira <j0n4t@hotmail.com>
2022-09-25 15:40:12 -0300

    feat: Order e Product

---
+++ src/v3/Order.ts
@@ -0,0 +1,22 @@
+import { CPF } from "./CPF";
+import { Product } from "./Product";
+
+export class Order {
+  constructor(
+    readonly cpf: CPF,
+    readonly products: Product[],
+    readonly discount = 0
+  ) {}
+
+  get totalPrice() {
+    const subTotal = this.products.reduce(
+      (total, { price, quantity }) => total + price * quantity,
+      0
+    );
+    if (this.discount > 0) {
+      const discountDifference = this.discount / 100;
+      return Math.floor(subTotal * (1 - discountDifference) * 100) / 100;
+    }
+    return subTotal;
+  }
+}
---
+++ src/v3/Product.ts
@@ -0,0 +1,7 @@
+export class Product {
+  constructor(
+    readonly description: string,
+    readonly price: number,
+    readonly quantity: number
+  ) {}
+}
