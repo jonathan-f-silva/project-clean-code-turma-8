dbc338dfa821414a17206f9fe17f94e2468282dc
Jonathan Ferreira <j0n4t@hotmail.com>
2022-09-25 16:03:39 -0300

    feat(Product): adiciona validações de campos

--- src/v3/Product.ts
+++ src/v3/Product.ts
@@ -3,5 +3,21 @@ export class Product {
     readonly description: string,
     readonly price: number,
     readonly quantity: number
-  ) {}
+  ) {
+    if (!this.isValidDescription())
+      throw new Error("Erro: Produto com descrição inválida");
+    if (!this.isValidPrice())
+      throw new Error("Erro: Produto com preço inválido");
+    if (!this.isValidQuantity())
+      throw new Error("Erro: Produto com quantidade inválida");
+  }
+  private isValidDescription() {
+    return this.description !== "";
+  }
+  private isValidPrice() {
+    return this.price >= 0;
+  }
+  private isValidQuantity() {
+    return this.quantity >= 0;
+  }
 }
