fb4bdf506f8931607b00d33c2ec2dcd99a552ce7
Jonathan Ferreira <j0n4t@hotmail.com>
2022-09-25 16:03:28 -0300

    test(Product): adiciona validações de campos

--- test/v3/Product.test.ts
+++ test/v3/Product.test.ts
@@ -1,11 +1,26 @@
 import { Product } from "../../src/v3/Product";
 
 describe("Classe Product", () => {
-  test("Deve ter descrição, preço e quantidade)", () => {
+  test("Deve ter descrição, preço e quantidade", () => {
     const product = new Product("Bola", 1.7, 3);
-
     expect(product.description).toEqual("Bola");
     expect(product.price).toEqual(1.7);
     expect(product.quantity).toEqual(3);
+  });
+  test("Não deve cadastrar com descrição vazia", () => {
+    expect(() => new Product("", 0, 0)).toThrow(
+      "Erro: Produto com descrição inválida"
+    );
+  });
+
+  test("Não deve cadastrar com preço negativo", () => {
+    expect(() => new Product("Bola", -1, 0)).toThrow(
+      "Erro: Produto com preço inválido"
+    );
+  });
+  test("Não deve cadastrar com quantidade negativa", () => {
+    expect(() => new Product("Bola", 1, -10)).toThrow(
+      "Erro: Produto com quantidade inválida"
+    );
   });
 });
